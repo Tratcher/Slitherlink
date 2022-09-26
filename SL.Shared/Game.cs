b868c3184f3db44239adb2acc942dd9bbe16f767
Chris R <Tratcher@outlook.com>
2022-09-26 15:40:11 -0700

    Bypass validation on user input

--- SL.Shared/Game.cs
+++ SL.Shared/Game.cs
@@ -22,7 +22,7 @@ namespace SL.Shared
         public Stack<Move> History { get; } = new();
         public Stack<Move> RedoHistory { get; } = new();
 
-        public bool MarkCellEdge(int row, int column, int direction, bool? hasLine, bool preventOverride = true)
+        public bool MarkCellEdge(int row, int column, int direction, bool? hasLine, bool preventOverride = true, bool validate = true)
         {
             var cell = Board[row, column];
 
@@ -56,79 +56,80 @@ namespace SL.Shared
             }
 
             // Validations:
-
-            // - An adjacent cell would have too many lines
-            if (hasLine == true && cell.Hint == cell.Lines)
+            if (validate)
             {
-                throw new InvalidOperationException($"Cell at r:{row} c:{column} h:{cell.Hint} d:{direction} already has enough lines.");
-            }
-            // - or too few
-            if (hasLine == false && cell.Undetermined + cell.Lines - 1 < cell.Hint)
-            {
-                throw new InvalidOperationException($"Cell at r:{row} c:{column} h:{cell.Hint} d:{direction} wouldn't have enough lines.");
-            }
-            int adjacentRow = row, adjacentColumn = column;
-            switch (direction)
-            {
-                case Direction.North:
-                    adjacentRow--;
-                    break;
-                case Direction.South:
-                    adjacentRow++;
-                    break;
-                case Direction.East:
-                    adjacentColumn++;
-                    break;
-                case Direction.West:
-                    adjacentColumn--;
-                    break;
-            }
-            if (0 <= adjacentRow && adjacentRow < Board.Rows
-                && 0 <= adjacentColumn && adjacentColumn < Board.Columns)
-            {
-                var adjacentCell = Board[adjacentRow, adjacentColumn];
-                if (hasLine == true && adjacentCell.Hint == adjacentCell.Lines)
+                // - An adjacent cell would have too many lines
+                if (hasLine == true && cell.Hint == cell.Lines)
                 {
-                    throw new InvalidOperationException($"Cell at r:{adjacentRow} c:{adjacentColumn} h:{adjacentCell.Hint} d:{Direction.Opposite(direction)} already has enough lines.");
+                    throw new InvalidOperationException($"Cell at r:{row} c:{column} h:{cell.Hint} d:{direction} already has enough lines.");
                 }
-                if (hasLine == false && adjacentCell.Undetermined + adjacentCell.Lines - 1 < adjacentCell.Hint)
+                // - or too few
+                if (hasLine == false && cell.Undetermined + cell.Lines - 1 < cell.Hint)
                 {
-                    throw new InvalidOperationException($"Cell at r:{adjacentRow} c:{adjacentColumn} h:{adjacentCell.Hint} d:{Direction.Opposite(direction)} wouldn't have enough lines.");
+                    throw new InvalidOperationException($"Cell at r:{row} c:{column} h:{cell.Hint} d:{direction} wouldn't have enough lines.");
+                }
+                int adjacentRow = row, adjacentColumn = column;
+                switch (direction)
+                {
+                    case Direction.North:
+                        adjacentRow--;
+                        break;
+                    case Direction.South:
+                        adjacentRow++;
+                        break;
+                    case Direction.East:
+                        adjacentColumn++;
+                        break;
+                    case Direction.West:
+                        adjacentColumn--;
+                        break;
+                }
+                if (0 <= adjacentRow && adjacentRow < Board.Rows
+                    && 0 <= adjacentColumn && adjacentColumn < Board.Columns)
+                {
+                    var adjacentCell = Board[adjacentRow, adjacentColumn];
+                    if (hasLine == true && adjacentCell.Hint == adjacentCell.Lines)
+                    {
+                        throw new InvalidOperationException($"Cell at r:{adjacentRow} c:{adjacentColumn} h:{adjacentCell.Hint} d:{Direction.Opposite(direction)} already has enough lines.");
+                    }
+                    if (hasLine == false && adjacentCell.Undetermined + adjacentCell.Lines - 1 < adjacentCell.Hint)
+                    {
+                        throw new InvalidOperationException($"Cell at r:{adjacentRow} c:{adjacentColumn} h:{adjacentCell.Hint} d:{Direction.Opposite(direction)} wouldn't have enough lines.");
+                    }
+                }
+
+                // - The junction already has two lines
+                int junction1Row = row, junction1Column = column, junction2Row = row, junction2Column = column;
+                switch (direction)
+                {
+                    case Direction.North: // East
+                        junction2Column++;
+                        break;
+                    case Direction.South: // East from the south west junction
+                        junction1Row++;
+                        junction2Row++;
+                        junction2Column++;
+                        break;
+                    case Direction.East: // South from the north east junction
+                        junction1Column++;
+                        junction2Row++;
+                        junction2Column++;
+                        break;
+                    case Direction.West: // South from the north west junction
+                        junction2Row++;
+                        break;
+                }
+                var junction1 = Board.GetJunction(junction1Row, junction1Column);
+                if (hasLine == true && junction1.LineCount >= 2)
+                {
+                    throw new InvalidOperationException($"Junction at r:{junction1Row} c:{junction1Column} already has enough lines.");
+                }
+                var junction2 = Board.GetJunction(junction2Row, junction2Column);
+                if (hasLine == true && junction1.LineCount >= 2)
+                {
+                    throw new InvalidOperationException($"Junction at r:{junction2Row} c:{junction2Column} already has enough lines.");
                 }
             }
-
-            // - The junction already has two lines
-            int junction1Row = row, junction1Column = column, junction2Row = row, junction2Column = column;
-            switch (direction)
-            {
-                case Direction.North: // East
-                    junction2Column++;
-                    break;
-                case Direction.South: // East from the south west junction
-                    junction1Row++;
-                    junction2Row++;
-                    junction2Column++;
-                    break;
-                case Direction.East: // South from the north east junction
-                    junction1Column++;
-                    junction2Row++;
-                    junction2Column++;
-                    break;
-                case Direction.West: // South from the north west junction
-                    junction2Row++;
-                    break;
-            }
-            var junction1 = Board.GetJunction(junction1Row, junction1Column);
-            if (hasLine == true && junction1.LineCount >= 2)
-            {
-                throw new InvalidOperationException($"Junction at r:{junction1Row} c:{junction1Column} already has enough lines.");
-            }
-            var junction2 = Board.GetJunction(junction2Row, junction2Column);
-            if (hasLine == true && junction1.LineCount >= 2)
-            {
-                throw new InvalidOperationException($"Junction at r:{junction2Row} c:{junction2Column} already has enough lines.");
-            }
-
 
             // Make the change
 
@@ -148,7 +149,7 @@ namespace SL.Shared
             return true;
         }
 
-        public bool MarkJunctionEdge(int row, int column, int direction, bool? hasLine, bool preventOverride = true)
+        public bool MarkJunctionEdge(int row, int column, int direction, bool? hasLine, bool preventOverride = true, bool validate = true)
         {
             // Translate back into cell coordinates first.
             switch (direction)
@@ -156,27 +157,27 @@ namespace SL.Shared
                 case Direction.North:
                     if (column < Board.Columns)
                     {
-                        return MarkCellEdge(row - 1, column, Direction.West, hasLine, preventOverride);
+                        return MarkCellEdge(row - 1, column, Direction.West, hasLine, preventOverride, validate);
                     }
-                    return MarkCellEdge(row - 1, column - 1, Direction.East, hasLine, preventOverride);
+                    return MarkCellEdge(row - 1, column - 1, Direction.East, hasLine, preventOverride, validate);
                 case Direction.South:
                     if (column < Board.Columns)
                     {
-                        return MarkCellEdge(row, column, Direction.West, hasLine, preventOverride);
+                        return MarkCellEdge(row, column, Direction.West, hasLine, preventOverride, validate);
                     }
-                    return MarkCellEdge(row, column - 1, Direction.East, hasLine, preventOverride);
+                    return MarkCellEdge(row, column - 1, Direction.East, hasLine, preventOverride, validate);
                 case Direction.East:
                     if (row < Board.Rows)
                     {
-                        return MarkCellEdge(row, column, Direction.North, hasLine, preventOverride);
+                        return MarkCellEdge(row, column, Direction.North, hasLine, preventOverride, validate);
                     }
-                    return MarkCellEdge(row - 1, column, Direction.South, hasLine, preventOverride);
+                    return MarkCellEdge(row - 1, column, Direction.South, hasLine, preventOverride, validate);
                 case Direction.West:
                     if (row < Board.Rows)
                     {
-                        return MarkCellEdge(row, column - 1, Direction.North, hasLine, preventOverride);
+                        return MarkCellEdge(row, column - 1, Direction.North, hasLine, preventOverride, validate);
                     }
-                    return MarkCellEdge(row - 1, column - 1, Direction.South, hasLine, preventOverride);
+                    return MarkCellEdge(row - 1, column - 1, Direction.South, hasLine, preventOverride, validate);
                 default:
                     throw new NotImplementedException(direction.ToString());
             }
